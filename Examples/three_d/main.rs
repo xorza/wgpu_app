9ca5f0b2989e29603031ff6de52f86972de47ac5
xxorza <1442523+xorza@users.noreply.github.com>
2024-06-01 13:26:44 +0300

    Update render function in 3D example
    The render function within the 3D example has been updated to include a command encoder for rendering, as well as data for color attachments and operations. Additionally, the use statement at the top of the file now includes LoadOp, Operations, and StoreOp from the wgpu library.
    
    Signed-off-by: xxorza <1442523+xorza@users.noreply.github.com>

--- Examples/three_d/main.rs
+++ Examples/three_d/main.rs
@@ -1,3 +1,5 @@
+use wgpu::{LoadOp, Operations, StoreOp};
+
 use wgpu_app::events::{EventResult, WindowEvent};
 use wgpu_app::wgpu_app::{AppContext, WgpuApp};
 
@@ -25,12 +27,29 @@ impl WgpuApp for App {
         }
     }
 
-    fn render(&mut self, app_context: &AppContext, _surface_view: &wgpu::TextureView) {
-        let mut command_encoder =
+    fn render(&mut self, app_context: &AppContext, surface_view: &wgpu::TextureView) {
+        let mut encoder =
             app_context.device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
 
+        encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[
+                Some(wgpu::RenderPassColorAttachment {
+                    view: surface_view,
+                    resolve_target: None,
+                    ops: Operations {
+                        load: LoadOp::Clear(wgpu::Color { r: 0.1, g: 0.2, b: 0.3, a: 1.0 }),
+                        store: StoreOp::Store,
+                    },
+                }),
+            ],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
 
-        app_context.queue.submit([command_encoder.finish()]);
+
+        app_context.queue.submit([encoder.finish()]);
     }
 }
 
